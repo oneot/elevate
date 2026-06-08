29e6b2df1f4bde57a23085f6f836f18e72356470
YoonKeumJae <ykj1512@gmail.com>
2026-06-08 15:34:00 +0900

    feat: configure activity video storage

--- Elevate.Server/tests/config.test.js
+++ Elevate.Server/tests/config.test.js
@@ -9,3 +9,16 @@ test('local settings example documents the calendar events Cosmos container', ()
 
   assert.equal(settings.Values.COSMOS_CALENDAR_EVENTS_CONTAINER_NAME, 'posts');
 });
+
+test('local settings example documents the activity videos Cosmos container', () => {
+  const settingsPath = path.join(__dirname, '..', 'local.settings.json.example');
+  const settings = JSON.parse(fs.readFileSync(settingsPath, 'utf8'));
+
+  assert.equal(settings.Values.COSMOS_ACTIVITY_VIDEOS_CONTAINER_NAME, 'posts');
+});
+
+test('cosmos client exports getActivityVideosContainer', () => {
+  const cosmos = require('../src/services/cosmosClient');
+
+  assert.equal(typeof cosmos.getActivityVideosContainer, 'function');
+});
